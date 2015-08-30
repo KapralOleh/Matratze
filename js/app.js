a95aa92606b6987b248ea90f10d9d474ab7739ef
Oleh Kapral <kapraloleh@gmail.com>
2015-08-30 20:19:11 +0300

    gh-pages

--- js/app.js
+++ js/app.js
@@ -2,21 +2,6 @@ var app = angular.module('myApp', []);
 app.controller('customersCtrl', function($scope, $http) {
     $http.get("json/headers.json")
     .success(function(response) {$scope.headers = response});
-    $scope.show = false;
-	$scope.buttonShow = function () {
-		$scope.show = !$scope.show;
-	};	
 });
 
-app.controller("ReviewController", function(){
-  	this.review = {};
-  	
-  	this.addReview = function(product){
-  		//this.review = {createdOn: Date.now()};
-  		product.reviews.push(this.review);
-  		  	this.review = {};
-
-
-  	};
-  });
 
