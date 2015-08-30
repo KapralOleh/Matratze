995402d30cbe06816e2504f8c3d096a2411662a2
Oleh Kapral <kapraloleh@gmail.com>
2015-08-30 20:02:14 +0300

    gh-pages

--- js/app.js
+++ js/app.js
@@ -7,3 +7,16 @@ app.controller('customersCtrl', function($scope, $http) {
 		$scope.show = !$scope.show;
 	};	
 });
+
+app.controller("ReviewController", function(){
+  	this.review = {};
+  	
+  	this.addReview = function(product){
+  		//this.review = {createdOn: Date.now()};
+  		product.reviews.push(this.review);
+  		  	this.review = {};
+
+
+  	};
+  });
+
